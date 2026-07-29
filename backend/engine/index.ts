d950a47433ee9839c993a34b7c11e889af45bc08
Jake <ram19018@byui.edu>
2026-07-29 17:04:59 -0600

    action Registry stubbed

--- backend/engine/index.ts
+++ backend/engine/index.ts
@@ -3,5 +3,6 @@ export { Attributes } from "./model/actors/Attributes";
 export { VitalPools } from "./model/actors/VitalPools";
 export { Actor } from "./model/actors/Actor";
 export { Action } from "./model/actions/Action";
+export { ActionRegistry } from "./model/actions/ActionRegistry";
 export { Effect } from "./model/effects/Effect";
 export { ResourceCost } from "./valueObjects/ResourceCost";
